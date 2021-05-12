817258cd61d3cdd2dbde7fbaedde244cf0b27691
shotius <sh.archemashvili@gmail.com>
2021-05-12 21:14:15 +0400

    added font in utils

--- src/shared/components/Button/Styles.js
+++ src/shared/components/Button/Styles.js
@@ -1,31 +1,29 @@
 import styled, {css} from "styled-components";
-import {colors, mixins } from '../../utils/styles'
+import {colors, font, mixins } from '../../utils/styles'
 
 export const StyledButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 25px;
-  font-family: "Lato";
   background: none;
   border: none;
+  ${font.normal};
+  ${font.size(14)}
   ${mixins.clickable}
   ${props => buttonVariants[props.variant]}
 `;
 
 const navLink = css`
-  font-size: 14px;
   text-transform: uppercase;
-  font-family: 'Lato', sans-serif;
   margin-right: 31px;
   &:hover {
     border-bottom: 3px solid ${colors.darkPurple};
-    font-weight: bold;
+    ${font.bold}
   }
 `;
 
 const tagLink = css`
-  font-size: 14px;
   &:hover {
     background-color: red;
   }
@@ -37,7 +35,6 @@ const signUp = css`
   width: 69px;
   background-color: ${colors.darkPurple};
   color: white;
-  ${'' /* padding: 10px; */}
   border: 1px solid #80334B;
   border-radius: 5px;
   &:active {
@@ -53,6 +50,9 @@ const buttonVariants = {
   login: css`
     text-transform: capitalize;
     color: ${colors.darkPurple};
+    &:hover {
+      ${font.bold};
+    }
   `,
 }
 
--- src/shared/utils/styles.js
+++ src/shared/utils/styles.js
@@ -4,6 +4,12 @@ export const colors = {
     darkPurple: '#80334B',
 }
 
+export const font = {
+    normal: `font-family: Lato; font-weight: normal;`,
+    bold: `font-family: Lato; font-weight: bold;`,
+    size: (size) => `font-size: ${size}px;`
+}
+
 export const mixins = {
     clickable: css`
         cursor: pointer;
