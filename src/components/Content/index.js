af0438a4c8efe9da136474e9784a422baec96450
shotius <sh.archemashvili@gmail.com>
2021-05-12 11:52:45 +0400

    init commit

---
+++ src/components/Content/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <div>Content</div>
+    )
+}
+export default Content
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+    color: red;
+    text-align: center; 
+`;
+
+const Header = () => {
+    return (
+        <Heading>header</Heading>
+    )
+}
+export default Header
