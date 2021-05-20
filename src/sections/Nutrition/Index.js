4cb447c2185c615d30ff0782d56eea54cedc6af3
shotius <sh.archemashvili@gmail.com>
2021-05-20 13:09:28 +0400

    nutrigion is okay on the small screen

--- src/sections/Nutrition/Index.js
+++ src/sections/Nutrition/Index.js
@@ -1,24 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '../../shared/components/Button'
 import SectionHeader from '../../shared/components/SectionHeader'
-import { TextWrap, NutritionText, NutritionBackground, NutritionWrap, InfoWrap } from './Styles'
-import { ImageWrapper, Img, TagLinks} from '../Exercise/Styles'
+import { TextWrap, NutritionText, NutritionBackground, NutritionWrap, Nav, ImageWrap, InfoWrap, Header, Dots } from './Styles'
+import { Img } from '../Exercise/Styles'
 import food_1 from '../../shared/assests/Rectangle 1473.png'
+import { Dot } from '../Carousels/Slider/Styles'
 
 
 const Nutrition = React.forwardRef((props,  ref) => {
+    const [activeIndex, setActiveIndex] = useState(0)
+   
+    const data = [
+        {   
+            id: 0,
+            heading: "Healty",
+            img: '',
+            header: ' A short title works',
+            text: ' best At home to gym, crossfit to pilates, yoga to bodyweight, as well as healthy and easy to make breakfasts, lunches, dinners and snacks. We want to make sure you enjoy the journey for a healthy lifestyle.'
+        },
+        {
+            id: 1,
+            heading: 'Proffesioanl',
+            img: '',
+            header: 'Bla bla bla',
+            text: ' segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche'
+        },
+    ]
+
+
     return (    
         <NutritionBackground>
             <NutritionWrap>
-                <SectionHeader size="large" color="white">
+                <Header size="large" color="white">
                     Nutrition
-                </SectionHeader>
+                </Header>
                 <InfoWrap>
-                    <TagLinks>
-                        <Button type="tagLink" color='dirtyWhite' onhover="white">healthy</Button>
-                        <Button type="tagLink" color='dirtyWhite' onhover="white">healthy</Button>
-                        <Button type="tagLink" color='dirtyWhite' onhover="white">healthy</Button>
-                    </TagLinks>
+                    <Nav>
+                    {data.map(data => (
+                        <Button 
+                            key={data.id}
+                            type="tagLink" 
+                            color='dirtyWhite' 
+                            onhover="white"
+                            onClick={() => {}}
+                            >
+                                {data.heading}
+                        </Button>
+                    ))}
+                    </Nav>
                     <TextWrap color="white">
                         <SectionHeader size="medium" color="white">
                         A short title works
@@ -27,9 +56,12 @@ const Nutrition = React.forwardRef((props,  ref) => {
                             best At home to gym, crossfit to pilates, yoga to bodyweight, as well as healthy and easy to make breakfasts, lunches, dinners and snacks. We want to make sure you enjoy the journey for a healthy lifestyle.
                         </NutritionText>
                     </TextWrap>
-                    <ImageWrapper>
+                    <ImageWrap>
                         <Img src={food_1} />
-                    </ImageWrapper>
+                    </ImageWrap>
+                    <Dots>
+                        <Dot />
+                    </Dots>
                 </InfoWrap>
             </NutritionWrap>
         </NutritionBackground>
--- src/sections/Nutrition/Styles.js
+++ src/sections/Nutrition/Styles.js
@@ -1,8 +1,10 @@
+import { Image } from "@styled-icons/boxicons-regular";
 import styled, {css} from "styled-components/macro";
 import { StyledHeader } from "../../shared/components/SectionHeader/Styles";
 import { colors, devices, font } from "../../shared/utils/styles";
 import {Wrapper,StyledText} from '../../Styles'
-import { Dots } from '../Carousels/Slider/Styles'
+import { SectionDots } from "../Exercise/Styles";
+import { ImageWrapper } from "../SecondSection/Styles";
 
 export const NutritionBackground = styled.div`
     background-color: ${colors.darkBlue}
@@ -18,14 +20,8 @@ position: relative;
 width: 100%; */
 `;
 
-
-export const SectionDots = styled(Dots)`
-    bottom: 0px;
-    position: relative;
-    padding-top: 10px;
-    @media ${devices.tablet} {
-        display: none;
-    }
+export const Header = styled(StyledHeader)`
+    margin: 10px auto;
 `;
 
 export const InfoWrap = styled.div`
@@ -34,13 +30,28 @@ export const InfoWrap = styled.div`
     justify-content: center;
 `;
 
+export const Nav = styled.div`
+    flex-basis: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ImageWrap = styled(ImageWrapper)`
+    order: 1;
+`;
+
 export const TextWrap = styled.div`
-    
+    order: 2;
 `;
 
 export const NutritionText = styled(StyledText)`
-color: ${colors.dirtyWhite};
-@media ${devices.tablet} {
-    color: ${colors.white};
-}
+    color: ${colors.dirtyWhite};
+    @media ${devices.tablet} {
+        color: ${colors.white};
+    }
+`;
+
+export const Dots = styled(SectionDots)`
+    order: 3;
 `;
