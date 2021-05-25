294978fdd917cecd1365c3da6f61366d298e8ff8
shotius <sh.archemashvili@gmail.com>
2021-05-25 16:18:04 +0400

    added photo in nutrition slide

--- src/sections/Nutrition/Index.js
+++ src/sections/Nutrition/Index.js
@@ -3,6 +3,7 @@ import Button from '../../shared/components/Button'
 import { TextWrap, NutritionText, NutritionBackground, NutritionWrap, Nav, ImageWrap, TextHeader, Header} from './Styles'
 import { Img, SectionDots } from '../Exercise/Styles'
 import food_1 from '../../shared/assests/Rectangle 1473.png'
+import food_2 from '../../shared/assests/food_2.png'
 import { Dot } from '../Carousels/Slider/Styles'
 
 
@@ -13,14 +14,14 @@ const Nutrition = React.forwardRef((props,  ref) => {
         {   
             id: 0,
             heading: "Healty",
-            img: '',
+            img: food_1,
             header: ' A short title works',
             text: ' best At home to gym, crossfit to pilates, yoga to bodyweight, as well as healthy and easy to make breakfasts, lunches, dinners and snacks. We want to make sure you enjoy the journey for a healthy lifestyle.'
         },
         {
             id: 1,
             heading: 'Proffesioanl',
-            img: '',
+            img: food_2,
             header: 'Bla bla bla',
             text: ' segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche'
         },
@@ -48,7 +49,7 @@ const Nutrition = React.forwardRef((props,  ref) => {
                     ))}
                 </Nav>
                 <ImageWrap>
-                    <Img src={food_1} />
+                    <Img src={data[activeIndex].img} />
                 </ImageWrap>
                 <TextWrap color="white">
                     <TextHeader size="medium" color="white">
